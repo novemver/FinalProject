963df752fa0d4c595fc266d62a73f2236ac8ba5d
sebib96 <sebastiantellob96@hotmail.com>
2023-04-18 16:23:35 -0400

    Created angular front end, configured auth

---
+++ ngSunbeam/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ngSunbeam';
+
+constructor(
+  private auth: AuthService
+){}
+
+  ngOnInit() {
+    this.tempTestDeleteMeLater(); // DELETE LATER!!!
+  }
+
+  tempTestDeleteMeLater() {
+    this.auth.login('admin','test').subscribe({ // change username to match DB
+      next: (data) => {
+        console.log('Logged in:');
+        console.log(data);
+      },
+      error: (fail) => {
+        console.error('Error authenticating:')
+        console.error(fail);
+      }
+    });
+  }
+}
---
+++ ngSunbeam/src/app/services/auth.service.ts
@@ -0,0 +1,97 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/user';
+import { Observable, catchError, throwError, tap } from 'rxjs';
+import { Buffer } from 'buffer';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+
+  constructor(private http: HttpClient) {}
+
+
+  private url = environment.baseUrl;
+
+
+  register(user: User): Observable<User> {
+    return this.http.post<User>(this.url + 'register', user).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('AuthService.register(): error registering user.')
+        );
+      })
+    );
+  }
+
+  login(username: string, password: string): Observable<User> {
+    const credentials = this.generateBasicAuthCredentials(username, password);
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Basic ${credentials}`,
+        'X-Requested-With': 'XMLHttpRequest',
+      }),
+    };
+
+    return this.http.get<User>(this.url + 'authenticate', httpOptions).pipe(
+      tap((newUser) => {
+        localStorage.setItem('credentials', credentials);
+        return newUser;
+      }),
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('AuthService.login(): error logging in user.')
+        );
+      })
+    );
+  }
+
+  logout(): void {
+    localStorage.removeItem('credentials');
+  }
+
+  getLoggedInUser(): Observable<User> {
+    if (!this.checkLogin()) {
+      return throwError(() => {
+        new Error('Not logged in.');
+      });
+    }
+    let httpOptions = {
+      headers: {
+        Authorization: 'Basic ' + this.getCredentials(),
+        'X-Requested-with': 'XMLHttpRequest',
+      },
+    };
+    return this.http
+      .get<User>(this.url + 'authenticate', httpOptions)
+      .pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError(
+            () => new Error( 'AuthService.getUserById(): error retrieving user: ' + err )
+          );
+        })
+      );
+  }
+
+  checkLogin(): boolean {
+    if (localStorage.getItem('credentials')) {
+      return true;
+    }
+    return false;
+  }
+
+  generateBasicAuthCredentials(username: string, password: string): string {
+    return Buffer.from(`${username}:${password}`).toString('base64');
+  }
+
+  getCredentials(): string | null {
+    return localStorage.getItem('credentials');
+  }
+
+}
