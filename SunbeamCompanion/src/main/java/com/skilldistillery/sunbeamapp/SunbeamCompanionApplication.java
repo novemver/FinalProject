b98f4208463fa0ab7e861c1953a122b811807710
Dom <dominicjackss@gmail.com>
2023-04-18 10:07:47 -0700

    Create Spring Boot project for REST API

---
+++ SunbeamCompanion/src/main/java/com/skilldistillery/sunbeamapp/SunbeamCompanionApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.sunbeamapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunbeamCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunbeamCompanionApplication.class, args);
+	}
+
+}
---
+++ SunbeamCompanion/src/test/java/com/skilldistillery/sunbeamapp/SunbeamCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.sunbeamapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunbeamCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
