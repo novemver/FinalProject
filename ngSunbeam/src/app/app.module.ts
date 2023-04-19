826d3377ecd2cc61aaf55e1e7fe3b7aad379e099
sebib96 <sebastiantellob96@hotmail.com>
2023-04-19 18:51:47 -0400

    components stubbed out

--- ngSunbeam/src/app/app.module.ts
+++ ngSunbeam/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ClientPageComponent } from './components/client-page/client-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    ClientPageComponent
   ],
   imports: [
     BrowserModule,
