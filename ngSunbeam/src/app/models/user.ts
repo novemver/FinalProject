ef037a917f4ec92e0c5470d6e2f258b9331cea2f
sebib96 <sebastiantellob96@hotmail.com>
2023-04-19 19:54:12 -0400

    navbar stubbed, user model complete

--- ngSunbeam/src/app/models/user.ts
+++ ngSunbeam/src/app/models/user.ts
@@ -2,17 +2,44 @@ export class User {
   id: number;
   username: string;
   password: string;
+  enabled: boolean;
   role: string;
+  email: string;
+  phoneNumber: string;
+  firstName: string;
+  lastName: string;
+  image_url: string;
+  biography: string;
+  createDate: Date;
+  updateDate: Date;
 
   constructor(
     id: number = 0,
     username: string = "",
     password: string = "",
-    role: string = ""
+    enabled: boolean = false,
+    role: string = "",
+    email: string = "",
+    phoneNumber: string = "",
+    firstName: string = "",
+    lastName: string = "",
+    image_url: string = "",
+    biography: string = "",
+    createDate: Date = new Date(),
+    updateDate: Date = new Date()
   ){
     this.id = id;
     this.username = username;
     this.password = password;
+    this.enabled = enabled;
     this.role = role;
+    this.email = email;
+    this.phoneNumber = phoneNumber;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.image_url = image_url;
+    this.biography = biography;
+    this.createDate = createDate;
+    this.updateDate = updateDate;
   }
 }
