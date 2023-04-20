f7ece93b7ec00e35e600471cd3fdc82d4fe56517
Dom <dominicjackss@gmail.com>
2023-04-20 13:11:26 -0700

    Refactor 'Client' to 'Elder', stub repo

---
+++ SunbeamCompanion/src/main/java/com/skilldistillery/sunbeamapp/repositories/ElderRepository.java
@@ -0,0 +1,10 @@
+package com.skilldistillery.sunbeamapp.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.sunbeamapp.entities.Elder;
+
+public interface ElderRepository extends JpaRepository<Elder, Integer> {
+
+	Elder findElderByUsername(String username);
+}
