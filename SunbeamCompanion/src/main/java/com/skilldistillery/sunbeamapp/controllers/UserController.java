088874560831ec39866f0f3d51c55c8e1ccfee1b
Dom <dominicjackss@gmail.com>
2023-04-22 10:32:36 -0700

    User can be archived/unarchived; Elder refactored to include enabled

--- SunbeamCompanion/src/main/java/com/skilldistillery/sunbeamapp/controllers/UserController.java
+++ SunbeamCompanion/src/main/java/com/skilldistillery/sunbeamapp/controllers/UserController.java
@@ -7,9 +7,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -82,5 +84,18 @@ public class UserController {
 			}
 			return archived;
 		}
+	
+	@PatchMapping("users/admin/{userId}")
+	public User unarchiveUser(@PathVariable int userId, HttpServletResponse res) {
+		User unArchive = userService.getUserById(userId);
+		if(userService.unarchiveUser(userId)) {
+			res.setStatus(200);
+		} else {
+			res.setStatus(404);
+		}
+		return unArchive;
+	    
+	}
+
 		
 }
