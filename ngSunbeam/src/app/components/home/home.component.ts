bb78e24caff715a7052fd80f5f7bade5e67410e1
Gerardo Quintero <jerryquinteroIII@Andrews-Mini.attlocal.net>
2023-04-23 10:31:44 -0600

    front-end-work

--- ngSunbeam/src/app/components/home/home.component.ts
+++ ngSunbeam/src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +11,46 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+
+users:User[] = [];
+showList: boolean = false;
+loggedInUser: User| null = null;
+
+
+  constructor(
+    private router: Router,
+    private userService:UserService,
+    private authService:AuthService
+  ){
+
+  }
+  ngOnInit() {
+
+    // this.authService.getLoggedInUser().subscribe({
+    //   next: (user: User) => {
+    //     this.loggedInUser = user;
+    //   },
+    //   error: (nojoy) => {
+    //     console.log(nojoy);
+    //   }
+
+    //  });
+
+    this.loadUsers();
+  }
+  viewUserProfile(user:User) {
+    this.router.navigate(['/user-profile', user.id]);
+
+  }
+  loadUsers(){
+    this.userService.getUsers().subscribe({
+      next: (users:User[]) => {
+        this.users = users;
+      },
+      error: (nojoy) => {
+        console.log(nojoy);
+      }
+    });
+
+  }
 }
