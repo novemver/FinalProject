4c35d5ca096eea3249de3dbb1c874121bc54de16
Gerardo Quintero <jerryquinteroIII@Andrews-Mini.attlocal.net>
2023-04-23 17:05:18 -0600

    registration is good to go

--- SunbeamCompanion/src/main/java/com/skilldistillery/sunbeamapp/controllers/AuthController.java
+++ SunbeamCompanion/src/main/java/com/skilldistillery/sunbeamapp/controllers/AuthController.java
@@ -16,7 +16,7 @@ import com.skilldistillery.sunbeamapp.services.AuthService;
 
 
 @RestController
-@CrossOrigin({ "*", "http://localhost/" })
+@CrossOrigin({ "*", "http://localhost:4200" })
 public class AuthController {
 	
 	@Autowired
