391973350925f08d451dae818392625644696edc
Dom <dominicjackss@gmail.com>
2023-04-24 22:00:28 -0700

    Stub elder/client profile

--- ngSunbeam/src/app/models/note.ts
+++ ngSunbeam/src/app/models/note.ts
@@ -1,2 +1,22 @@
 export class Note {
+  id: number;
+  title: string;
+  description: string;
+  flagged: string;
+  createDate: Date;
+
+constructor(
+  id: number = 0,
+  title: string = '',
+  description: string = '',
+  flagged: string = '',
+  createDate: Date = new Date()
+){
+  this.id = id;
+  this.title = title;
+  this.description = description;
+  this.flagged = flagged;
+  this.createDate = createDate;
+}
+
 }
