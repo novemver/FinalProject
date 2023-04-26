97e0872981e358111a526ef5ff1fa5319e0067d4
sebib96 <sebastiantellob96@hotmail.com>
2023-04-25 22:33:50 -0400

    ADD FORM IS WORKING :DDDDDDDD

--- ngSunbeam/src/app/components/client-page/client-page.component.ts
+++ ngSunbeam/src/app/components/client-page/client-page.component.ts
@@ -4,7 +4,7 @@ import { ElderService } from './../../services/elder.service';
 import { Component, OnInit } from '@angular/core';
 import { Elder } from 'src/app/models/elder';
 import { AuthService } from 'src/app/services/auth.service';
-import { User } from "src/app/models/user";
+import { User } from 'src/app/models/user';
 import { Appointment } from 'src/app/models/appointment';
 import { Note } from 'src/app/models/note';
 import { ReminderService } from 'src/app/services/reminder.service';
@@ -13,35 +13,39 @@ import { Medication } from 'src/app/models/medication';
 import { catchError, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
-
 @Component({
   selector: 'app-client-page',
   templateUrl: './client-page.component.html',
-  styleUrls: ['./client-page.component.css']
+  styleUrls: ['./client-page.component.css'],
 })
-export class ClientPageComponent implements OnInit{
-
+export class ClientPageComponent implements OnInit {
   selected: Elder | null = null;
 
   user: User | null = null;
 
   elders: Elder[] = [];
 
-  reminder: Reminder[] =[];
+  reminder: Reminder[] = [];
 
   newReminder: Reminder = new Reminder();
 
   newMedication: Medication = new Medication();
 
-  medication: Medication[] =[];
+  newAppointment: Appointment = new Appointment();
+
+  medication: Medication[] = [];
+
+  selectedForm: boolean = false;
 
   private url = environment.baseUrl + 'api/';
 
-
-  constructor(private auth: AuthService, private elderService: ElderService,
+  constructor(
+    private auth: AuthService,
+    private elderService: ElderService,
     private reminderService: ReminderService,
     private medicationService: MedicationService,
-    private appointmentService: AppointmentService){}
+    private appointmentService: AppointmentService
+  ) {}
 
   ngOnInit(): void {
     this.loadElders();
@@ -61,7 +65,7 @@ export class ClientPageComponent implements OnInit{
     return options;
   }
 
-  loadElders(){
+  loadElders() {
     this.elderService.getEldersForUser().subscribe({
       next: (data) => {
         this.elders = data;
@@ -70,95 +74,106 @@ export class ClientPageComponent implements OnInit{
         }
       },
       error: (err) => {
-        console.error("Error loading  elders" + err);
-
-      }
+        console.error('Error loading  elders' + err);
+      },
     });
   }
 
-  updateSelected(){
-    this.elders.forEach(elder => {
+  updateSelected() {
+    this.elders.forEach((elder) => {
       if (elder.id === this.selected?.id) {
         this.selected = elder;
       }
     });
   }
 
-    loadReminder(){
-      this.reminderService.getReminder().subscribe({
-        next: (data) => {
-          this.reminder = data;
-        },
-        error: (errr) => {
-          console.error("Error loading reminders" + errr)
-        }
-      })
-    }
+  loadReminder() {
+    this.reminderService.getReminder().subscribe({
+      next: (data) => {
+        this.reminder = data;
+      },
+      error: (errr) => {
+        console.error('Error loading reminders' + errr);
+      },
+    });
+  }
 
-    createReminder(){
-      this.reminderService.addReminder(this.newReminder).subscribe({
-        next: (newOne) => {
-          this.newReminder = new Reminder();
-        },
-        error: (ohno) => {
-          console.error("error creating reminder " + ohno)
-        }
-      })
-    }
+  createReminder() {
+    this.reminderService.addReminder(this.newReminder).subscribe({
+      next: (newOne) => {
+        this.newReminder = new Reminder();
+      },
+      error: (ohno) => {
+        console.error('error creating reminder ' + ohno);
+      },
+    });
+  }
 
-    loadMedication(){
-      this.medicationService.getMedication().subscribe({
-        next: (data) => {
-          this.medication = data;
-        },
-        error: (errr) => {
-          console.error("Error loading medications" + errr)
-        }
-      })
-    }
-    createMedication(){
-      this.medicationService.addMedication(this.newMedication).subscribe({
-        next: (newOne) => {
-          this.newMedication = new Medication();
-        },
-        error: (ohno) => {
-          console.error("error creating Medication " + ohno)
-        }
-      })
-    }
+  loadMedication() {
+    this.medicationService.getMedication().subscribe({
+      next: (data) => {
+        this.medication = data;
+      },
+      error: (errr) => {
+        console.error('Error loading medications' + errr);
+      },
+    });
+  }
+  createMedication() {
+    this.medicationService.addMedication(this.newMedication).subscribe({
+      next: (newOne) => {
+        this.newMedication = new Medication();
+      },
+      error: (ohno) => {
+        console.error('error creating Medication ' + ohno);
+      },
+    });
+  }
 
-    deleteAppointment(appointmentId: number) {
-      this.appointmentService.deleteAppointment(appointmentId).subscribe({
-        next: () => {
-          if (this.selected?.appointments) {
-            for (let index = 0; index < this.selected?.appointments?.length; index++) {
-              if (this.selected.appointments[index].id == appointmentId) {
-                this.selected.appointments.splice(index,1);
-              }
+  deleteAppointment(appointmentId: number) {
+    this.appointmentService.deleteAppointment(appointmentId).subscribe({
+      next: () => {
+        if (this.selected?.appointments) {
+          for (
+            let index = 0;
+            index < this.selected?.appointments?.length;
+            index++
+          ) {
+            if (this.selected.appointments[index].id == appointmentId) {
+              this.selected.appointments.splice(index, 1);
             }
           }
-        },
-        error: (didNotWork) => {
-          console.log('Error handiling delete');
-          console.error(didNotWork);
-        },
-      });
-    }
+        }
+      },
+      error: (didNotWork) => {
+        console.log('Error handiling delete');
+        console.error(didNotWork);
+      },
+    });
+  }
 
-      deleteReminder(reminderId: number) {
-        this.reminderService.destroy(reminderId).subscribe({
-          next: () => {
-           this.loadElders();
-          },
-          error: (didNotWork) => {
-            console.log('Error handiling delete');
-            console.error(didNotWork);
-          },
-        });
-      }
+  deleteReminder(reminderId: number) {
+    this.reminderService.destroy(reminderId).subscribe({
+      next: () => {
+        this.loadElders();
+      },
+      error: (didNotWork) => {
+        console.log('Error handiling delete');
+        console.error(didNotWork);
+      },
+    });
+  }
 
-      deleteMedication(medicationId: number){
+  deleteMedication(medicationId: number) {}
 
-      }
-
+  addAppointment(){
+    this.appointmentService.createAppointment(this.newAppointment, this.selected!.id).subscribe({
+      next: () => {
+        this.loadElders();
+      },
+      error: (err) => {
+        console.error("Error creating appointment" + err);
+      },
+    });
+  }
 }
